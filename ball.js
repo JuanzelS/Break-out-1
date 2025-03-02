b48f3a35b4f3ee778e1d98d5326951f2c3920d1a
JuanzelS <143665768+JuanzelS@users.noreply.github.com>
2025-03-02 18:06:15 -0500

    Update ball.js

--- ball.js
+++ ball.js
@@ -5,46 +5,16 @@ class Ball {
     this.y = canvas.height - 30;
     this.dx = 2;
     this.dy = -2;
-    this.canvas = canvas;
+    this.color = "#0095DD";
   }
 
-  move() {
-    this.x += this.dx;
-    this.y += this.dy;
-
-    // Ball wall collision
-    if (this.x + this.dx > this.canvas.width - this.radius || this.x + this.dx < this.radius) {
-      this.dx = -this.dx;
-    }
-    if (this.y + this.dy < this.radius) {
-      this.dy = -this.dy;
-    }
-    if (this.y + this.dy > this.canvas.height - this.radius) {
-      if (this.x > paddle.x && this.x < paddle.x + paddle.width) {
-        this.dy = -this.dy;
-      } else {
-        // Ball hits the bottom (game over or restart logic can go here)
-      }
-    }
-  }
-
-  checkCollision(paddle) {
-    if (
-      this.y + this.dy > paddle.y - this.radius &&
-      this.y + this.dy < paddle.y + paddle.height &&
-      this.x > paddle.x &&
-      this.x < paddle.x + paddle.width
-    ) {
-      this.dy = -this.dy;
-    }
-  }
-
-  draw(ctx) {
+  render(ctx) {
     ctx.beginPath();
     ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2);
-    ctx.fillStyle = "#0095DD";
+    ctx.fillStyle = this.color;
     ctx.fill();
     ctx.closePath();
   }
 }
 
+export default Ball;  // Export the class
