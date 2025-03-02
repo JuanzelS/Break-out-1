17212d8b5af1a48049df5415e3a053c1a18c1fa0
JuanzelS <143665768+JuanzelS@users.noreply.github.com>
2025-03-02 18:06:42 -0500

    Update game.js

--- game.js
+++ game.js
@@ -1,31 +1,23 @@
+import Paddle from "./paddle.js";
+import Ball from "./ball.js";
+
 class Game {
   constructor(paddle, ball, ctx) {
     this.paddle = paddle;
     this.ball = ball;
     this.ctx = ctx;
-    this.intervalId = null;
   }
 
   start() {
-    this.intervalId = setInterval(() => {
-      this.update();
-      this.draw();
-    }, 1000 / 60); // 60 FPS
+    this.render();
   }
 
-  stop() {
-    clearInterval(this.intervalId);
-  }
-
-  update() {
-    this.ball.move();
-    this.paddle.move();
-    this.ball.checkCollision(this.paddle);
-  }
-
-  draw() {
-    this.ctx.clearRect(0, 0, canvas.width, canvas.height); // Clear canvas
-    this.ball.draw(this.ctx);
-    this.paddle.draw(this.ctx);
+  render() {
+    this.ctx.clearRect(0, 0, canvas.width, canvas.height);
+    this.paddle.render(this.ctx);
+    this.ball.render(this.ctx);
+    requestAnimationFrame(() => this.render());
   }
 }
+
+export default Game;  // Export the class
