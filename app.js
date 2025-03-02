976ce7d46a73035a07cb4fdb8b38ddec5bf4e0b0
JuanzelS <143665768+JuanzelS@users.noreply.github.com>
2025-03-02 17:55:22 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,9 @@
+const canvas = document.getElementById("gameCanvas");
+const ctx = canvas.getContext("2d");
+
+const paddle = new Paddle(canvas);
+const ball = new Ball(canvas);
+const game = new Game(paddle, ball, ctx);
+
+game.start();
 
