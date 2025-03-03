9a42f2070b63a0a0134fd182c333d63891043ff6
JuanzelS <143665768+JuanzelS@users.noreply.github.com>
2025-03-02 19:22:14 -0500

    Update paddle.js

--- paddle.js
+++ paddle.js
@@ -1,16 +1,14 @@
-class Paddle {
+export class Paddle {
   constructor(canvas) {
     this.width = 75;
     this.height = 10;
     this.x = (canvas.width - this.width) / 2;
-    this.y = canvas.height - this.height - 10;
-    this.color = "#0095DD";
+    this.canvas = canvas;
+    this.ctx = canvas.getContext("2d");
   }
 
-  render(ctx) {
-    ctx.fillStyle = this.color;
-    ctx.fillRect(this.x, this.y, this.width, this.height);
+  draw() {
+    this.ctx.fillStyle = "#0095DD";
+    this.ctx.fillRect(this.x, this.canvas.height - this.height - 10, this.width, this.height);
   }
 }
-
-export default Paddle;  // Export the class
