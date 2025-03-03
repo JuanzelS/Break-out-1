e6c25fb203b1ce51ab87fb52c900616ef074e066
JuanzelS <143665768+JuanzelS@users.noreply.github.com>
2025-03-02 19:22:53 -0500

    Update game.js

--- game.js
+++ game.js
@@ -1,23 +1,21 @@
-import Paddle from "./paddle.js";
-import Ball from "./ball.js";
-
-class Game {
-  constructor(paddle, ball, ctx) {
+export class Game {
+  constructor(canvas, paddle, ball, ctx) {
+    this.canvas = canvas;
     this.paddle = paddle;
     this.ball = ball;
     this.ctx = ctx;
   }
 
   start() {
-    this.render();
+    this.update();
   }
 
-  render() {
-    this.ctx.clearRect(0, 0, canvas.width, canvas.height);
-    this.paddle.render(this.ctx);
-    this.ball.render(this.ctx);
-    requestAnimationFrame(() => this.render());
+  update() {
+    this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
+    this.paddle.draw();
+    this.ball.draw();
+    this.ball.update();
+
+    requestAnimationFrame(() => this.update());
   }
 }
-
-export default Game;  // Export the class
