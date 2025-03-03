0daaf1cec2c4133fc720c2526f6e0add4b1dfbe8
JuanzelS <143665768+JuanzelS@users.noreply.github.com>
2025-03-02 19:26:25 -0500

    Update app.js

--- app.js
+++ app.js
@@ -2,11 +2,13 @@ import { Game } from "./game.js";
 import { Paddle } from "./paddle.js";
 import { Ball } from "./ball.js";
 
-const canvas = document.getElementById("gameCanvas");
-const ctx = canvas.getContext("2d");
+document.addEventListener("DOMContentLoaded", () => {
+    const canvas = document.getElementById("gameCanvas");
+    const ctx = canvas.getContext("2d");
 
-const paddle = new Paddle(canvas);
-const ball = new Ball(canvas);
-const game = new Game(canvas, paddle, ball, ctx);
+    const paddle = new Paddle(canvas);
+    const ball = new Ball(canvas);
+    const game = new Game(canvas, paddle, ball, ctx);
 
-game.start();
+    game.start();
+});
