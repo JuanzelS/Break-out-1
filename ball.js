1e52c53374e270f941c3d85b1ea8222d77819e0c
JuanzelS <143665768+JuanzelS@users.noreply.github.com>
2025-03-02 19:31:16 -0500

    Update ball.js

--- ball.js
+++ ball.js
@@ -1,24 +1,61 @@
 export class Ball {
-  constructor(canvas) {
-    this.radius = 10;
-    this.x = canvas.width / 2;
-    this.y = canvas.height - 30;
-    this.dx = 2;
-    this.dy = -2;
-    this.canvas = canvas;
-    this.ctx = canvas.getContext("2d");
-  }
+    constructor(canvas, paddle) {
+        this.canvas = canvas;
+        this.paddle = paddle;
+        this.radius = 5;
+        this.x = canvas.width / 2;
+        this.y = canvas.height - 30;
+        this.dx = 2;
+        this.dy = -2;
+    }
 
-  draw() {
-    this.ctx.beginPath();
-    this.ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2);
-    this.ctx.fillStyle = "#0095DD";
-    this.ctx.fill();
-    this.ctx.closePath();
-  }
+    update() {
+        this.x += this.dx;
+        this.y += this.dy;
 
-  update() {
-    this.x += this.dx;
-    this.y += this.dy;
-  }
+        // Wall Collision
+        if (this.x < this.radius || this.x > this.canvas.width - this.radius) {
+            this.dx *= -1;
+        }
+        if (this.y < this.radius) {
+            this.dy *= -1;
+        }
+
+        // Paddle Collision
+        if (
+            this.y + this.radius >= this.canvas.height - this.paddle.height - 10 &&
+            this.x >= this.paddle.x &&
+            this.x <= this.paddle.x + this.paddle.width
+        ) {
+            this.dy *= -1;
+        }
+    }
+
+    reset() {
+        this.x = this.canvas.width / 2;
+        this.y = this.canvas.height - 30;
+        this.dx = 2;
+        this.dy = -2;
+    }
+
+    hitBrick(brick) {
+        if (
+            this.x > brick.x &&
+            this.x < brick.x + brick.width &&
+            this.y > brick.y &&
+            this.y < brick.y + brick.height
+        ) {
+            this.dy *= -1;
+            return true;
+        }
+        return false;
+    }
+
+    draw(ctx) {
+        ctx.fillStyle = "white";
+        ctx.beginPath();
+        ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2);
+        ctx.fill();
+        ctx.closePath();
+    }
 }
