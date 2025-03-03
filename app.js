636516fc55023fd667308cdfdaa33073a1c0dac7
JuanzelS <143665768+JuanzelS@users.noreply.github.com>
2025-03-02 19:21:54 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
-import Paddle from "./paddle.js";
-import Ball from "./ball.js";
-import Game from "./game.js";
+import { Game } from "./game.js";
+import { Paddle } from "./paddle.js";
+import { Ball } from "./ball.js";
 
 const canvas = document.getElementById("gameCanvas");
 const ctx = canvas.getContext("2d");
 
 const paddle = new Paddle(canvas);
 const ball = new Ball(canvas);
-const game = new Game(paddle, ball, ctx);
+const game = new Game(canvas, paddle, ball, ctx);
 
 game.start();
